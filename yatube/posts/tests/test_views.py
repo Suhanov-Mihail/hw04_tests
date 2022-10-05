6c29c88c2cb3cf7fe56f1a5e8cf89cd86e9d90e7
Михаил Суханов <m9032352211@yandex.ru>
2022-10-06 00:38:11 +0300

    один

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -46,15 +46,17 @@ class VIEWSTests(TestCase):
         templates_pages_names = {
             'posts/index.html': reverse('posts:index'),
             'posts/group_list.html': (reverse('posts:group_list',
-                                        kwargs={'slug': 'test-slug'})),
+                                     kwargs={'slug': 'test-slug'})),
             'posts/profile.html': (reverse('posts:profile',
-                                     kwargs={'username': 'SnoopDog2'})),
+                                  kwargs={'username': 'SnoopDog2'})),
             'posts/post_detail.html': (reverse('posts:post_detail',
-                                         kwargs={'post_id': self.post.pk})),
-            'posts/create_post.html': (reverse('posts:post_edit', kwargs={'post_id': self.post.pk})),
+                                      kwargs={'post_id': self.post.pk})),
+            'posts/create_post.html': (reverse('posts:post_edit',
+                                      kwargs={'post_id': self.post.pk})),
             'posts/create_post.html': reverse('posts:post_create')
         }
-        # Проверяем, что при обращении к name вызывается соответствующий HTML-шаблон
+        # Проверяем, что при обращении к name 
+        # вызывается соответствующий HTML-шаблон
         for template, reverse_name in templates_pages_names.items():
             with self.subTest(reverse_name=reverse_name):
                 response = self.authorized_client.get(reverse_name)
